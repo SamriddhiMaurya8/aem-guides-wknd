ea3b264ad6ffe89b13e96bd7c29e951125bb362e
Samriddhi <samriddhi.maurya@axeno.co>
2024-10-05 11:35:16 +0530

    updated ripple js

--- ui.frontend/src/main/webpack/components/_ripplecomponent.js
+++ ui.frontend/src/main/webpack/components/_ripplecomponent.js
@@ -1,43 +1,39 @@
+const rippleItems = document.querySelectorAll('.rippleEffect__rippleCarousel--rippleItem');
+const controlLeft = document.querySelector('.control-left');
+const controlRight = document.querySelector('.control-right');
+let currentRippleIndex = 0;
 
+function updateRippleCarousel() {
+  rippleItems.forEach((rippleItem, index) => {
+    if (index < currentRippleIndex || index >= currentRippleIndex + 4) {
+      rippleItem.classList.add('hidden');
+    } else {
+      rippleItem.classList.remove('hidden');
 
-// const items = document.querySelectorAll('.rippleEffect__rippleCarousel--rippleItem');
-// const leftButton = document.querySelector('.control-left');
-// const rightButton = document.querySelector('.control-right');
-// let currentIndex = 0;
+      if (index % 2 === 0) {
+        rippleItem.style.transform = 'translateY(50px)';
+      } else {
+        rippleItem.style.transform = 'translateY(-50px)';
+      }
+    }
+  });
 
-// function updateCarousel() {
-//   items.forEach((item, index) => {
-//     if (index < currentIndex || index >= currentIndex + 4) {
-//       item.classList.add('hidden');
-//     } else {
-//       item.classList.remove('hidden');
+  controlLeft.disabled = currentRippleIndex === 0;
+  controlRight.disabled = currentRippleIndex + 4 >= rippleItems.length;
+}
 
+controlLeft.addEventListener('click', () => {
+  if (currentRippleIndex > 0) {
+    currentRippleIndex -= 1;
+    updateRippleCarousel();
+  }
+});
 
-//       if (index % 2 === 0) {
-//         item.style.transform = 'translateY(50px)'; 
-//       } else {
-//         item.style.transform = 'translateY(-50px)'; 
-//       }
-//     }
-//   });
+controlRight.addEventListener('click', () => {
+  if (currentRippleIndex + 4 < rippleItems.length) {
+    currentRippleIndex += 1;
+    updateRippleCarousel();
+  }
+});
 
-//   leftButton.disabled = currentIndex === 0;
-//   rightButton.disabled = currentIndex + 4 >= items.length;
-// }
-
-// leftButton.addEventListener('click', () => {
-//   if (currentIndex > 0) {
-//     currentIndex -= 1;
-//     updateCarousel();
-//   }
-// });
-
-// rightButton.addEventListener('click', () => {
-//   if (currentIndex + 4 < items.length) {
-//     currentIndex += 1;
-//     updateCarousel();
-//   }
-// });
-
-
-// updateCarousel();
+updateRippleCarousel();
